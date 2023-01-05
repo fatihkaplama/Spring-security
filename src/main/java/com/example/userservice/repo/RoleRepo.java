f3ff471399c0c1727b99a5ff859c98fbb85579f4
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-06 00:31:22 +0300

    Add user and role repositories

---
+++ src/main/java/com/example/userservice/repo/RoleRepo.java
@@ -0,0 +1,8 @@
+package com.example.userservice.repo;
+
+import com.example.userservice.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepo extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}

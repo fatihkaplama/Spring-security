cea9c0dbdbd271671b74cb104e51b190e85252e5
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-06 01:27:17 +0300

    Add command line runner

--- src/main/java/com/example/userservice/UserserviceApplication.java
+++ src/main/java/com/example/userservice/UserserviceApplication.java
@@ -1,13 +1,33 @@
 package com.example.userservice;
 
+import com.example.userservice.model.Role;
+import com.example.userservice.model.User;
+import com.example.userservice.service.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserserviceApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(UserService userService){
+		return args -> {
+			userService.saveRole(new Role(null, "ROLE_USER"));
+			userService.saveRole(new Role(null, "ROLE_MANAGER"));
+			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+			userService.saveRole(new Role(null, "ROLE_SUPER_ADMIN"));
+
+			userService.saveUser(new User(null, "Fatih Kaplama", "fatihkaplama", "123", new ArrayList<>()));
+			userService.addRoleToUser("fatihkaplama", "ROLE_USER");
+		};
 	}
 
 }

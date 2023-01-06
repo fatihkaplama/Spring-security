6f646d2b6fc224e78fa38aa47fe968d96567d989
Fatih Kaplama <63496655+fatihkaplama@users.noreply.github.com>
2023-01-07 01:25:44 +0300

    Add security config

--- src/main/java/com/example/userservice/service/UserServiceImplementation.java
+++ src/main/java/com/example/userservice/service/UserServiceImplementation.java
@@ -6,18 +6,40 @@ import com.example.userservice.repo.RoleRepo;
 import com.example.userservice.repo.UserRepo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 @Slf4j
-public class UserServiceImplementation implements UserService{
+public class UserServiceImplementation implements UserService, UserDetailsService {
     private final UserRepo userRepo;
     private final RoleRepo roleRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User not found in the database");
+        } else {
+            log.info("User found: {}", username);
+        }
+        Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
+        });
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+    }
+
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {}", user.getName());
